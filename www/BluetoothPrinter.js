7da8e107ebd8e640518d02a33aad32eb66e059e2
antero <anterodev@gmail.com>
2017-01-08 00:48:05 -0300

    fix define

--- www/BluetoothPrinter.js
+++ www/BluetoothPrinter.js
@@ -1,33 +1,30 @@
-cordova.define("cordova-plugin-bluetooth-printer.BluetoothPrinter", function (require, exports, module) {
-    var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-    var BTPrinter = {
-        list: function (fnSuccess, fnError) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
-        },
-        connect: function (fnSuccess, fnError, name) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [name]);
-        },
-        disconnect: function (fnSuccess, fnError) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "disconnect", []);
-        },
-        print: function (fnSuccess, fnError, str) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
-        },
-        printText: function (fnSuccess, fnError, str) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
-        },
-        printImage: function (fnSuccess, fnError, str) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "printImage", [str]);
-        },
-        printPOSCommand: function (fnSuccess, fnError, str) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "printPOSCommand", [str]);
-        },
-        printQRCode: function (fnSuccess, fnError, str) {
-            exec(fnSuccess, fnError, "BluetoothPrinter", "printQRCode", [str]);
-        }
-    };
+var BTPrinter = {
+    list: function (fnSuccess, fnError) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
+    },
+    connect: function (fnSuccess, fnError, name) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [name]);
+    },
+    disconnect: function (fnSuccess, fnError) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "disconnect", []);
+    },
+    print: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
+    },
+    printText: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
+    },
+    printImage: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printImage", [str]);
+    },
+    printPOSCommand: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printPOSCommand", [str]);
+    },
+    printQRCode: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printQRCode", [str]);
+    }
+};
 
-    module.exports = BTPrinter;
-
-});
+module.exports = BTPrinter;
